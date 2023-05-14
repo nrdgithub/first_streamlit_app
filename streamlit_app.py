5c094acd5ca8545e072ef0d09d249f04a9cdb24e
nrdgithub <133595369+nrdgithub@users.noreply.github.com>
2023-05-14 23:25:04 +0530

    Update streamlit_app.py
    added read_csv function to read CSV file

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+

10e3a53b4060a3e06deaabe3171f38fd82a26be4
nrdgithub <133595369+nrdgithub@users.noreply.github.com>
2023-05-14 23:07:44 +0530

    Update streamlit_app.py
    new line of code added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('hard-Bolied Free-Range Egg')
+
